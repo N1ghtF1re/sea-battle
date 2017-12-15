8f84837b20c713d72a91bb4003e641636530df91
Alexandr Pankratiew <30345819+N1ghtF1re@users.noreply.github.com>
2017-12-15 18:24:58 +0300

    Update Nikita.pas

--- Sources/units/Nikita.pas
+++ Sources/units/Nikita.pas
@@ -8,49 +8,27 @@ interface
 Implementation
 
   procedure Readfile(var Field1,field2:TPlayingField);
-  var c:char;
-  var f:file of char;
-  var i,j:integer;
+  var f,g: file of char;
+  var i,j: integer;
+  var c: char;
   begin
     assign(f,link1);
-    reset(f);
-    for i :=1 to 10 do
-    begin
+    reset(f); 
+    assign(g,link2);
+    reset(g);
+    for i:=1 to 10 do
+    begin 
       for j:=1 to 10 do
-      begin
-        read(f,Field1[i,j]);
-      end;
-      read(f,c);
-      read(f,c);
+      read(g,Field2[i,j]);
+      read(f,Field1[i,j]);
     end;
-    {for i :=1 to 10 do
-  begin
-    for j:=1 to 10 do
-     write(Field1[i,j]);
-    writeln;
-    end;  }
-    assign(f,link2);
-    reset(f);
-    for i :=1 to 10 do
+    if i<>10 THEN
     begin
-      for j:=1 to 10 do
-      begin
-        read(f,Field2[i,j]);
-      end;
       read(f,c);
-      read(f,c);
+      read(f,c); 
+      read(g,c);
+      read(g,c);
     end;
-
-   {for i :=1 to 10 do
-  begin
-    for j:=1 to 10 do
-    begin
-     write(Field2[i,j]);
-    end;
-      writeln;
-    end;        }
-
   end;
 
 end.
-
